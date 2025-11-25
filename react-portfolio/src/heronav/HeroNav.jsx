5b7544ab3404720b7ab73a1fc6c9dbac87e0abf7
koujkarima-lab <koujkarima@gmail.com>
2025-11-25 23:11:13 +0100

    HeroNav.jsx done

---
+++ react-portfolio/src/heronav/HeroNav.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const HeroNav = () => {
+  return (
+    <header className="header">
+    <nav className="header__nav">
+      <div>
+        <h1>KR</h1>
+      </div>
+      <div className="navbar">
+        <a href="">HOME</a>
+        <a href="">ABOUT</a>
+        <a href="">PROJECTS</a>
+        <a href="">CONTACT</a>
+      </div>
+      <div className="icon">
+        <i className="fa-brands fa-facebook-f"></i>
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-linkedin"></i>
+      </div>
+      <div className="nav__menu"> <i className="fa-solid fa-list" style={{ color: "red" }}>Hello</i> <a href=""></a>
+      </div>
+    </nav>
+    <div className="Header__center">
+      <div className="info">
+        <h1>Katie Reed</h1>
+        <h4>Web Developer & Designer</h4>
+        <div><button>CONTACT ME</button></div>
+      </div>
+    </div>
+  </header>
+  )
+}
+
+export default HeroNav
