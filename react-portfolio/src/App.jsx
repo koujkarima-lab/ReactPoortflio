4484164110f353dd0bc394e380a6932e2c44acb2
koujkarima-lab <koujkarima@gmail.com>
2025-11-25 23:15:17 +0100

    AboutMe.jsx done

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 import './App.css'
 import HeroNav from './heronav/HeroNav'
+import AboutMe from './heronav/aboutme/AboutMe'
 
 function App() {
 
   return (
     <>
       <HeroNav />
+      <AboutMe />
     </>
   )
 }
---
+++ react-portfolio/src/heronav/aboutme/AboutMe.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AboutMe = () => {
+  return (
+<article className="first__section">
+    <h1>About me</h1>
+    <div className="section__about">
+      <p>
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+        sadipscing elitr, sed diam nonumy eirmod tempor.
+      </p>
+      <button>Download Resume</button>
+    </div>
+  </article>
+    )
+}
+
+export default AboutMe
